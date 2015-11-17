e01586dbe76cc62bbf481b712a59f46c45a67890
Brandon Smith <brandon93s@gmail.com>
2015-11-16 20:22:15 -0500

    Update preinstall hook to work on Windows and UNIX.

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
     resolve = require('path').resolve,
     join = require('path').join,
-    cp = require('child_process');
+    cp = require('child_process'),
+    exec = cp.exec;
 
 // logging helper function
 var msg = function (msg) {
@@ -9,23 +10,32 @@ var msg = function (msg) {
     console.log(div, msg, div, '\n');
 };
 
-// get library path
+
 var lib = resolve(__dirname, '../lib/');
-
-// get node modules path
 var node_modules = resolve(__dirname, '../node_modules');
+var symlinkPath = join(node_modules, '_');
 
-// setup symlink
-if (fs.existsSync(join(node_modules, '_'))) {
-    msg('Lib symlink found!');
-} else {
-    msg('Lib symlink missing! Adding symlink...');
-    cp.spawn('cmd', ['/c', 'mklink', '/D', '_', '..\lib'], {
-        env: process.env,
-        cwd: node_modules,
-        stdio: 'inherit'
-    });
-}
+// setup symlink from node_modules to lib
+fs.lstat(symlinkPath, function (err, stat) {
+    var createSymlink = function () {
+        fs.symlinkSync(lib, symlinkPath, 'dir');
+    };
+
+    if (err) {
+        if ('ENOENT' === err.code) {
+            msg('Creating Symlink to Lib...');
+            createSymlink();
+        } else {
+            console.log(err);
+        }
+    } else if (!stat.isSymbolicLink()) {
+        msg('File is not a Symlink. Replacing with Symlink...');
+        fs.unlinkSync(symlinkPath);
+        createSymlink();
+    } else {
+        msg('Symlink Found, Continuing...');
+    }
+});
 
 // npm install sub-modules
 fs.readdirSync(lib)
@@ -35,13 +45,22 @@ fs.readdirSync(lib)
         // ensure path has package.json
         if (!fs.existsSync(join(modPath, 'package.json'))) return;
 
-        // log status to console
-        msg('Installing ' + mod + ' dependencies');
-
-        // install folder
-        cp.spawn('cmd', ['/c', 'npm', 'i'], {
+        // exec npm install in sub-module
+        exec('npm install', {
             env: process.env,
             cwd: modPath,
-            stdio: 'inherit'
+        }, function (error, stdout, stderr) {
+
+            msg('Installing ' + mod + ' dependencies...');
+
+            if (stdout) {
+                console.log('stdout: ' + stdout);
+            }
+            if (stderr) {
+                console.log('stderr: ' + stderr);
+            }
+            if (error) {
+                console.log('exec error: ' + error);
+            }
         });
     });
