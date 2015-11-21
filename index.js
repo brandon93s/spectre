86f8fd05c8883bf9e0d796029338146d14bd4d72
Brandon Smith <brandon93s@gmail.com>
2015-11-21 16:40:08 -0500

    Implement workers and phantom child processes.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /* main module entry point */
 var cfg = require('_/config'),
     log = require('_/log'),
-    app = require('_/app');
+    app = require('_/app'),
+    workerFarm = require('_/farm');
 
 app.listen(cfg.port);
 log.info('app listening on port', cfg.port);
+
+workerFarm.boot();
